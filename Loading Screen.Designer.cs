04ba3926787e8f965803f62b5adb8c85e7a3afd9
Mitchell Clack <u21447234@up.ac.za>
2021-09-14 20:30:19 +0200

    done

--- Loading Screen.Designer.cs
+++ Loading Screen.Designer.cs
@@ -37,9 +37,9 @@ namespace INF164HWAss1
             // pictureBox1
             // 
             this.pictureBox1.Image = global::INF164HWAss1.Properties.Resources.Loading_Screen;
-            this.pictureBox1.Location = new System.Drawing.Point(-27, -2);
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(345, 357);
+            this.pictureBox1.Size = new System.Drawing.Size(336, 359);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -48,11 +48,12 @@ namespace INF164HWAss1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(344, 357);
+            this.ClientSize = new System.Drawing.Size(335, 357);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmLoadingScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.frmLoadingScreen_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
