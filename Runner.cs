d49d357939caaf369497c3875e7b93ba3e7fb8fb
Gerard Geldenhuys <gerardrx@outlook.com>
2021-11-02 22:25:06 +0200

    screen error fix

--- Runner.cs
+++ Runner.cs
@@ -39,9 +39,11 @@ namespace INF164HWAss1
 
             if (Screen.PrimaryScreen.WorkingArea.Size.Height < 1040 || Screen.PrimaryScreen.WorkingArea.Size.Width < 1920)
             {
-                MessageBox.Show("Your screen resolution is to low to render the game properly" +
+                MessageBox.Show("Your screen resolution is to low to render the game properly or your scale is more than 100%" +
                     "\nIf your screen is not 1920x1080 or more you cant play this game" +
-                    "\nPlease use a different monitor or have someone else grade this part of the game" +
+                    "\nIf your screen is 1920x1080 and you are seeing this message" +
+                    "\nset your scale to 100% in widows display settings"+
+                    "\nElse please use a different monitor or have someone else grade this part of the game" +
                     "\nIve tried everything to fix this, but since the game mechanics are" +
                     "\ntied to screen size there is nothing I can do");
             }
